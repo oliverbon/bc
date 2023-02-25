c03491576840844b0c0e1ad1bb6c9aa630be23d3
oliverbon <oliverboni@gmail.com>
2023-02-24 22:00:44 -0500

    Update app3.py

--- app3.py
+++ app3.py
@@ -18,8 +18,7 @@ WIDTH = 512 # pixels
 CLASS_NAMES = ["cancer", "no cancer"]
 
 def main():
-    pix = []
-    npix = []
+    npix = np.asarray([])
     st.write("# Deteccion de Cancer")
     with st.form("my-form",clear_on_submit=True):
         uploaded_file = st.file_uploader("Choose a DCM file", type=['png', 'jpg','dcm'])
