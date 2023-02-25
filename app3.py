7c0acadaaac65b3ad5b1ea958ac814db430538f8
oliverbon <oliverboni@gmail.com>
2023-02-24 20:58:51 -0500

    Update app3.py

--- app3.py
+++ app3.py
@@ -19,7 +19,7 @@ CLASS_NAMES = ["cancer", "no cancer"]
 def main():
     st.write("# Breast Cancer Detection")
     with st.form("my-form",clear_on_submit=True):
-        uploaded_file = st.file_uploader("Choose a DCM file", type=['png', 'jpg','dcm'])
+        uploaded_file = st.file_uploader("Choose a DCM, png or jpg file", type=['png', 'jpg','dcm'])
         submitted = st.form_submit_button("Submit")
         if submitted:
             if uploaded_file is not None:
