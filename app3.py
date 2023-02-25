b0e6159b64898b3d6ae0e0effdba563c811daa42
oliverbon <oliverboni@gmail.com>
2023-02-25 17:22:19 -0500

    Update app3.py

--- app3.py
+++ app3.py
@@ -15,6 +15,7 @@ CLASS_NAMES = ["cancer", "no cancer"]
 def main():
     st.write("# Deteccion de Cancer")
     with st.form("my-form",clear_on_submit=True):
+        print(uploaded_file.type)
         uploaded_file = st.file_uploader("Choose a DCM file", type=['png', 'jpg','dcm'])
         submitted = st.form_submit_button("Submit")
         if submitted:
